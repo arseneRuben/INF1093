b707afa86f49f4ea148ffde1d0f357c2b011babd
arseneruben <fopoar@gmail.com>
2024-08-06 14:41:09 -0400

    reprise

--- Semaine1/Search.py
+++ Semaine1/Search.py
@@ -1,54 +1,5 @@
-## Exercice 1
-#### Question 1
-listIdName = [(202401, "Anani"), (202402, "Simon"),(202403, "Pierre"),(202404, "Kylian"),(202405, "Alphonse"),(202406, "Joshua"),(202407, "Colince"),(202408, "Khristian"),(202409, "Zinedine"),(202410, "Didier") ]
-print(listIdName)
-
-
-## Exercice 2
-#### Question 1
-def searchNameById(name):
-    ## Completer ici 
-
-    return 0
-
-print("Le matricule de Anani : ", searchNameById("Anani"))  ## Le matricule de Anani :  202401 
-print("Le matricule de Peter : ", searchNameById("Peter"))  ## Le matricule de Peter :  0
-
-#### Question 2
-def searchGradeByIdSeq(id):
-    ## Completer ici 
-
-    return 0
-  
-print("La note du matricule 202403 est de  : ", searchGradeByIdSeq(202403))  ## La note du matricule 202403 est de  :  73
-print("La note du matricule 202405 est de  : ", searchGradeByIdSeq(202405))  ## La note du matricule 202403 est de  :  0
-
-#### Question 3
-def buildListSeq():
-    ## Completer ici 
-    return 0
-    
-buildListSeq()
-print(fullList)
-
-## Exercice 3
-## Question1  : implementons le tri de ton choix
-
-
-
-    
-listIdGrade = tri_selection(listIdGrade)
-#print(listIdGrade)
-
-####Question 2
-def searchGradeByIdDicho(id):
- 
-    return 0
-
-## Question 3
-def buildListDicho():
-
-    return 0
-    
-buildListDicho()
-#print(fullList)
+def indice(elt, lst):
+       for i in range(len(lst)):
+            if lst[i] == elt:
+                return i
+       return None
